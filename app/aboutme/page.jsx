f37f6193c1a6f1bce36f282ded17e21d3001077f
Eddu <55552484+St4rman@users.noreply.github.com>
2025-08-17 15:53:18 +0100

    sword

--- app/aboutme/page.jsx
+++ app/aboutme/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import localFont from 'next/font/local';
+import Model from "@/components/Model.jsx";
 
 const test = [
 "1", "2", "3"
@@ -19,17 +20,26 @@ const AboutMe = () => {
         <div className='text-3xl lg:text-7xl'>I AM A </div>
         <div className='text-5xl lg:text-9xl text-accent italic' >GAME PROGRAMMER</div>
       </div>
-      <div className='flex flex-col justify-end items-end my-10'>
-        <div className='max-w-3/4 lg:max-w-3/5 text-left my-3 px-5 lg:mx-20 lg:px-20'>
-          I am a UK based Indian Gameplay Programmer, currently looking for work. I'm driven and skilled with Unity, and Unreal.
+      
+        <div className='flex justify-center align-top flex-col lg:flex-row mx-15'>
+         <div className='min-w-l lg:min-w-xl'>
+          <Model /> 
+         </div>
+          <div className='flex flex-col lg:px-1'>
+              <div className='text-left my-3 '>
+              * I am a UK based Gameplay Programmer, currently looking for work. I'm driven and skilled with Unity, and Unreal.
+              </div>
+              <div className='text-left my-3'>
+              * During the past 3+ years of my career (that I spent between jambox games, Rockstar games, and as  VR gameplay programmer) I specialized in player facing features, gameplay programming & game design.
+              </div>
+              <div className='text-left my-3'>
+              * During my offtime, I enjoy art, TTRPGs and going on deep dives about coffee on wikipedia at 5AM. 
+              </div>
+          </div>
         </div>
-        <div className='max-w-3/4 lg:max-w-3/5 text-left my-3 px-5 lg:mx-20 lg:px-20'>
-          During the past 3+ years of my career (that I spent between jambox games, Rockstar games, and as  VR gameplay programmer) I specialized in player facing features, gameplay programming & game design.
-        </div>
-        <div className='max-w-3/4 lg:max-w-3/5 text-left my-3 px-5 lg:mx-20 lg:px-20'>
-          During my offtime, I enjoy art, TTRPGs and going on deep dives about coffee on wikipedia at 5AM. 
-        </div>
-      </div>
+    
+
+     
     </>
   )
 }
---
+++ components/Model.jsx
@@ -0,0 +1,48 @@
+import { useFrame } from '@react-three/fiber'
+import React from 'react';
+import { useRef } from "react";
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+
+const Model =() =>{
+    return (
+        <>
+        <div className='h-[30vh] lg:h-[50vh]'>
+        <Canvas >  
+            < Scene/>
+            <ambientLight intensity={0.9} />
+        </Canvas>
+        </div>
+        
+        </>
+    );
+}
+
+export default Model;
+
+function Scene() {
+    
+    const gltf = useLoader(GLTFLoader, 'assets/scene.glb')
+    const myMesh = React.useRef();
+
+  
+
+    useFrame(({clock}) => {
+        const delta = Math.sin(clock.elapsedTime);
+        myMesh.current.position.y += delta* 0.005;
+        myMesh.current.rotation.z -= 0.01;
+    })
+
+
+    return (
+        <>
+        <mesh ref={myMesh} position={[-10,-1,-10]} rotation={[90, 90, 0]}>
+             <primitive object={gltf.scene} scale={1.3} />
+        </mesh>
+           
+        </>
+   
+);
+}
